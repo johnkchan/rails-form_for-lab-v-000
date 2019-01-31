a99903019949000a954df00b50df1d2e32fea9c9
Jonathan Chan <johnkchan252@gmail.com>
2019-01-31 04:32:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,10 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :school_classes do |t|
-      t.string :first_name
-      t.string :last_name
-
-      t.timestamps null: false
+      t.string :title
+      t.integer :room_number
     end
   end
 end
